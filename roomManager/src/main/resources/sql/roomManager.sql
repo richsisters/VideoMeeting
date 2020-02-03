8531c084717e8041ed93e527a0974fc257fe9833
wangmengyuan <879992488@qq.com>
2020-02-03 22:28:17 +0800

    add resource

--- roomManager/src/main/resources/sql/roomManager.sql
+++ roomManager/src/main/resources/sql/roomManager.sql
@@ -5,7 +5,7 @@
 create sequence user_info_uid_seq start with 10001;
 create sequence user_info_rid_seq start with 20001;
 create table user_info (
-  uid bigint primary key not null default user_info_uid_seq.nextval,
+  uid bigint not null default user_info_uid_seq.nextval primary key ,
   user_name varchar(100) not null,
   password varchar(100) not null,
   roomId bigint not null default user_info_rid_seq.nextval,
