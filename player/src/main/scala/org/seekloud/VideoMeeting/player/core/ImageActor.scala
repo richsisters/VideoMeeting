0dbf0e98209a8de34fbfdc0ffe02a7f6ef2d8de4
“shuai1126” <1042820762@qq.com>
2020-03-02 20:27:09 +0800

    .

--- player/src/main/scala/org/seekloud/VideoMeeting/player/core/ImageActor.scala
+++ player/src/main/scala/org/seekloud/VideoMeeting/player/core/ImageActor.scala
@@ -181,13 +181,10 @@ object ImageActor {
       val sH = gc.getCanvas.getHeight
       val w = img.getWidth
       val h = img.getHeight
-      if (id.contains("--")) { //三人连线状态
-//        if (w / sW > h / sH) {
+      if(id.contains("---")){ //四人连线状态
+        gc.drawImage(img, sW / 2, sH / 2, sW / 2, sH / 2)
+      }else if (id.contains("--")) { //三人连线状态
           gc.drawImage(img, 0, sH / 2, sW / 2, sH / 2)
-//        } else {
-//          gc.drawImage(img, (sW - w * sH / h) / 2 + sW / 2, 0, (w * sH / h) / 2, sH / 2)
-//        }
-
       }else if (id.contains("-")) { //连线状态
         if (w / sW > h / sH) {
           gc.drawImage(img, sW / 2, 0, sW / 2, sH / 2)
@@ -195,8 +192,6 @@ object ImageActor {
           gc.drawImage(img, (sW - w * sH / h) / 2 + sW / 2, 0, (w * sH / h) / 2, sH / 2)
         }
 
-      } else if(id.contains("---")){ //四人连线状态
-        gc.drawImage(img, sW / 2, sH / 2, sW / 2, sH / 2)
       }
       else { //普通观看
         if (w / sW > h / sH) {
